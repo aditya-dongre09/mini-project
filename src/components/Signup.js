636fa4bfc031367c327fc5abfd73175e760a22a5
aditya-dongre09 <72660230+aditya-dongre09@users.noreply.github.com>
2021-10-07 11:53:43 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -32,7 +32,7 @@ export default function Signup() {
     }
 
     return (<>
-        <Card style={{minWidth: '400px'}}>
+        <Card style={{maxWidth: '400px'}}>
             <Card.Body>
                 <h2 className="text-center mb-4">Sign up</h2>
                 {error && <Alert variant="danger">{error}</Alert>} 
