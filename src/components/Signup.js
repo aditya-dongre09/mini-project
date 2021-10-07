8999aebb648ba86f509343fb1d3468d70bb235d9
aditya-dongre09 <72660230+aditya-dongre09@users.noreply.github.com>
2021-10-07 12:00:28 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -32,7 +32,8 @@ export default function Signup() {
     }
 
     return (<>
-        <Card style={{margin:'30px auto',maxWidth: '400px'}}>
+        <div style={{margin:'30px auto',maxWidth: '400px'}}>
+        <Card>
             <Card.Body>
                 <h2 className="text-center mb-4">Sign up</h2>
                 {error && <Alert variant="danger">{error}</Alert>} 
@@ -56,6 +57,7 @@ export default function Signup() {
         <div className="w-100 text-center mt-2">
             Already have an account? <Link to='/Login'>Log In!</Link>
             
+        </div>
         </div>
         </>
     )
